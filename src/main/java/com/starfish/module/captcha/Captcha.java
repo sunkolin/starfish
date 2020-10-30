57201b6ba5537ad7d782576a6efcb82ecb2463a8
sunlin <sunlin@kingsoft.com>
2020-10-30 13:55:36 +0800

    fix

--- src/main/java/com/starfish/module/captcha/Captcha.java
+++ src/main/java/com/starfish/module/captcha/Captcha.java
@@ -1,12 +1,14 @@
 package com.starfish.module.captcha;
 
+import cn.hutool.captcha.CaptchaUtil;
+
 /**
- * Captcha TODO
+ * Captcha
  *
  * @author sunny
  * @version 1.0.0
  * @since 2020-10-30
  */
-public class Captcha {
+public class Captcha extends CaptchaUtil {
 
 }
