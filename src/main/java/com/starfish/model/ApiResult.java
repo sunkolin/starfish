1562ac918185343cb6b932324352efec59b0e62a
sunlin <sunlin@kingsoft.com>
2020-10-30 15:01:38 +0800

    fix

---
+++ src/main/java/com/starfish/model/ApiResult.java
@@ -0,0 +1,14 @@
+package com.starfish.model;
+
+import java.io.Serializable;
+
+/**
+ * ApiResult
+ *
+ * @author sunny
+ * @version 1.0.0
+ * @since 2020-10-30
+ */
+public class ApiResult<T> extends Result<T> implements Serializable {
+
+}
