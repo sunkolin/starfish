06fae6e488517b7c7629823f5547ae27647ba99e
sunlin <sunlin@kingsoft.com>
2020-10-30 13:17:08 +0800

    fix

---
+++ src/main/java/com/starfish/module/express/Express.java
@@ -0,0 +1,12 @@
+package com.starfish.module.express;
+
+/**
+ * Express TODO
+ *
+ * @author sunny
+ * @version 1.0.0
+ * @since 2020-10-30
+ */
+public final class Express {
+
+}
---
+++ src/main/java/com/starfish/module/tr/TrafficRestriction.java
@@ -0,0 +1,25 @@
+package com.starfish.module.tr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TrafficRestriction
+ *
+ * @author sunny
+ * @version 1.0.0
+ * @since 2020-10-30
+ */
+public final class TrafficRestriction {
+
+    /**
+     * 尾号限行 TODO
+     * 接口文档：http://xianxing.loopon.cn/home/api
+     *
+     * @return 结果
+     */
+    public static List<Integer> getTrafficRestriction(String cityName) {
+        return new ArrayList<>();
+    }
+
+}
