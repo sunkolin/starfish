72ce1a43c776bc8f23b9d5c9dffbc4d5c2057e1c
sunlin <sunlin@kingsoft.com>
2021-02-28 10:48:43 +0800

    fix

--- src/main/java/com/starfish/extension/spring/CustomApplicationListener.java
+++ src/main/java/com/starfish/extension/spring/CustomApplicationListener.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.ServletContext;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * application start listener
@@ -22,39 +24,20 @@ import javax.servlet.ServletContext;
  * @version 1.0.0
  * @since 2014-12-08
  */
-@Component
 @Slf4j
 @SuppressWarnings(value = "unused")
-public abstract class AbstractApplicationListener implements org.springframework.context.ApplicationListener<ApplicationEvent>, ApplicationContextAware, InitializingBean {
+@Component
+public class CustomApplicationListener implements org.springframework.context.ApplicationListener<ApplicationEvent>, ApplicationContextAware, InitializingBean {
 
     private ApplicationContext applicationContext;
 
     /**
-     * execute after bean initialized
-     */
-    public abstract void init();
-
-    /**
-     * execute after spring start complete
-     *
-     * @param event event
-     */
-    public abstract void execute(ContextRefreshedEvent event);
-
-    public void setStartTime() {
-        if (applicationContext instanceof WebApplicationContext) {
-            ServletContext context = ((WebApplicationContext) applicationContext).getServletContext();
-            String startTime = String.valueOf(System.currentTimeMillis());
-            context.setAttribute("startTime", startTime);
-            log.info("ApplicationListener setStartTime={}", startTime);
-        }
-    }
-
-    /**
      * spring container event
-     * 在web项目中（spring mvc），系统会存在两个容器，一个是root application context ,另一个就是我们自己的 projectName-servlet context
-     * （作为root application context的子容器）,这种情况下，就会造成onApplicationEvent方法被执行两次。为了避免上面提到的问题，
-     * 我们可以只在root application context初始化完成后调用逻辑代码，其他的容器的初始化完成，则不做任何处理，修改后代码，可以用event判断容器
+     * 在web项目中（spring mvc），系统会存在两个容器，一个是root application context,
+     * 另一个就是我们自己的 projectName-servlet context（作为root application context的子容器）,
+     * 这种情况下，就会造成onApplicationEvent方法被执行两次。为了避免上面提到的问题，
+     * 我们可以只在root application context初始化完成后调用逻辑代码，其他的容器的初始化完成，则不做任何处理，
+     * 修改后代码，可以用event判断容器
      *
      * @param event event
      */
@@ -71,8 +54,8 @@ public abstract class AbstractApplicationListener implements org.springframework
                 log.info("root container start complete");
             } else {
                 log.info("web container start complete");
-                //somethings you want to do after spring start complete
-                execute((ContextRefreshedEvent) event);
+                // somethings you want to do after spring start complete
+                executeAfterContextRefreshedEvent((ContextRefreshedEvent) event);
             }
         } else if (event instanceof ContextStoppedEvent) {
             if (((ContextStoppedEvent) event).getApplicationContext().getParent() == null) {
@@ -89,9 +72,25 @@ public abstract class AbstractApplicationListener implements org.springframework
         }
     }
 
+    /**
+     * execute after spring start complete
+     *
+     * @param event event
+     */
+    public void executeAfterContextRefreshedEvent(ContextRefreshedEvent event) {
+        // execute after ContextRefreshedEvent
+        // 打印启动时间
+        long startupDate = applicationContext.getStartupDate();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String applicationStartupTime = format.format(new Date(startupDate));
+        log.info("application startup time={}", applicationStartupTime);
+    }
+
+
     @Override
     public void afterPropertiesSet() throws Exception {
-        init();
+        // init
+
     }
 
     @Override
@@ -99,8 +98,14 @@ public abstract class AbstractApplicationListener implements org.springframework
         this.applicationContext = applicationContext;
     }
 
-    public ApplicationContext getApplicationContext() {
-        return applicationContext;
-    }
+//    private void setStartTime() {
+//        if (applicationContext instanceof WebApplicationContext) {
+//            ServletContext context = ((WebApplicationContext) applicationContext).getServletContext();
+//            String startTime = String.valueOf(System.currentTimeMillis());
+//            context.setAttribute("startTime", startTime);
+//            log.info("ApplicationListener setStartTime={}", startTime);
+//            applicationContext.getStartupDate();
+//        }
+//    }
 
 }
