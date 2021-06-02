e737ff7b845fc046d4786f933383293738916f1b
sunlin <sunlin@kingsoft.com>
2021-06-02 17:59:56 +0800

    增加默认线程池

--- src/main/java/com/starfish/config/ExecutorAutoConfiguration.java
+++ src/main/java/com/starfish/config/ExecutorAutoConfiguration.java
@@ -1,4 +1,4 @@
-package com.starfish.extension.executor;
+package com.starfish.config;
 
 import com.google.common.base.Strings;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +20,7 @@ import java.util.concurrent.RejectedExecutionHandler;
  */
 @Slf4j
 @Configuration
-@ConditionalOnProperty(value = {"application.executor.enabled"}, matchIfMissing = false)
+@ConditionalOnProperty(value = {"application.executor.enabled"})
 @EnableConfigurationProperties({ExecutorProperties.class})
 public class ExecutorAutoConfiguration {
 
--- src/main/java/com/starfish/config/ExecutorProperties.java
+++ src/main/java/com/starfish/config/ExecutorProperties.java
@@ -1,4 +1,4 @@
-package com.starfish.extension.executor;
+package com.starfish.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
