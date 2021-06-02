ce617bd083633ad84b02af1591cd02f25a8e29e5
sunlin <sunlin@kingsoft.com>
2021-06-02 14:58:27 +0800

    fix

--- src/main/java/com/starfish/model/ApiResult.java
+++ src/main/java/com/starfish/model/ApiResult.java
@@ -1,5 +1,8 @@
 package com.starfish.model;
 
+import com.starfish.enumeration.ResultEnum;
+import com.starfish.exception.CustomException;
+
 import java.io.Serializable;
 
 /**
@@ -10,5 +13,25 @@ import java.io.Serializable;
  * @since 2020-10-30
  */
 public class ApiResult<T> extends Result<T> implements Serializable {
+
+    public ApiResult() {
+        super();
+    }
+
+    public ApiResult(T body) {
+        super(body);
+    }
+
+    public ApiResult(ResultEnum resultEnum) {
+        super(resultEnum);
+    }
+
+    public ApiResult(CustomException exception) {
+        super(exception);
+    }
+
+    public ApiResult(Integer status, String message) {
+        super(status, message);
+    }
 
 }
