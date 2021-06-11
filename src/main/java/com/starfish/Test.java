dcdf49f52391dc2597a2d580fdfe391857bad7db
sunlin <sunlin@kingsoft.com>
2021-06-11 16:10:47 +0800

    fix

--- src/main/java/com/starfish/Test.java
+++ src/main/java/com/starfish/Test.java
@@ -1,14 +1,5 @@
 package com.starfish;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.google.protobuf.Api;
-import com.starfish.model.ApiResult;
-import com.starfish.plus.CachePlus;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 /**
  * Test
  *
@@ -19,18 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class Test {
 
     public static void main(String[] args) {
-        Cache<Object, Object> caffeineCache = Caffeine.newBuilder().expireAfterWrite(3600, TimeUnit.SECONDS).build();
 
-        CachePlus cachePlus = new CachePlus(caffeineCache);
-//        Object o =  caffeineCache.getIfPresent("abc");
-//        System.out.println(o);
-
-
-        boolean b =  cachePlus.exist("abc");
-        System.out.println(b);
-        Object a = null;
-
-        List c = (List) a;
     }
 
 }
