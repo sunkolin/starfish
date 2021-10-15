c44f01084c43e9c9e1c4652e82e89bc182e413d1
sunlin <sunlin@kingsoft.com>
2021-10-15 09:07:58 +0800

    加上单元测试

--- src/test/java/com/starfish/util/DateTimeUtilTest.java
+++ src/test/java/com/starfish/util/DateTimeUtilTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -16,10 +17,46 @@ import java.util.List;
 public class DateTimeUtilTest {
 
     @Test
-    public void dateBetween() {
-        List<String> dates = DateTimeUtil.dateBetween("2014-01-01", "2014-01-03");
+    public void parseTest() {
+        Date date = DateTimeUtil.parse("2020-01-01");
+        System.out.println(date);
+        Assert.assertNotNull(date);
+
+        Date date2 = DateTimeUtil.parse("2020-01-01 12:00:00");
+        System.out.println(date2);
+        Assert.assertNotNull(date2);
+
+        Date date3 = DateTimeUtil.parse("2020-01-01");
+        System.out.println(date3);
+        Assert.assertNotNull(date3);
+
+        Date date4 = DateTimeUtil.parse("20200101");
+        System.out.println(date4);
+        Assert.assertNotNull(date4);
+
+        Date date5 = DateTimeUtil.parse("2020-01-01 12:00:00", "yyyy-MM-dd HH:mm:ss");
+        System.out.println(date5);
+        Assert.assertNotNull(date5);
+
+        Date date6 = DateTimeUtil.parse(1634106492000L);
+        System.out.println(date6);
+        Assert.assertNotNull(date6);
+    }
+
+    @Test
+    public void dateBetweenTest() {
+        List<String> dates = DateTimeUtil.dateBetween("2020-01-01", "2020-01-03");
         System.out.println(dates);
-        Assert.assertEquals(dates, Arrays.asList("2014-01-01", "2014-01-02", "2014-01-03"));
+        Assert.assertEquals(dates, Arrays.asList("2020-01-01", "2020-01-02", "2020-01-03"));
+    }
+
+    @Test
+    public void getMonthStartTest() {
+        // 时间是2020-10-15 09:00:00，当月第一天是2020-10-01 00:00:00，时间戳是1601481600000
+        Date date = new Date(1602723600000L);
+        Date monthStart = DateTimeUtil.getMonthStart(date);
+        System.out.println(monthStart);
+        Assert.assertEquals(monthStart.getTime(), 1601481600000L);
     }
 
 }
