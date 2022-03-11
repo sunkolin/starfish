972eff1feb86d0a37f62f9a6e5ebb867c7baf2ec
sunlin <sunlin@kingsoft.com>
2022-03-11 13:47:32 +0800

    直接使用hutool工具类

---
+++ src/main/java/com/starfish/util/ZipUtil.java
@@ -0,0 +1,38 @@
+package com.starfish.util;
+
+/**
+ * ZipUtil
+ *
+ * @author sunkolin
+ * @version 1.0.0
+ * @since 2022-03-11
+ */
+public class ZipUtil {
+
+    private ZipUtil() {
+
+    }
+
+    /**
+     * 压缩文件
+     * ZipUtil.zip("d:/aaa", "d:/bbb/aaa.zip");
+     *
+     * @param source 源
+     * @param target 目标
+     */
+    public static void zip(String source, String target) {
+        cn.hutool.core.util.ZipUtil.zip(source, target);
+    }
+
+    /**
+     * 解压文件
+     * File unzip = ZipUtil.unzip("E:\\aaa\\test.zip", "e:\\aaa");
+     *
+     * @param source 源
+     * @param target 目标
+     */
+    public static void unzip(String source, String target) {
+        cn.hutool.core.util.ZipUtil.unzip(source, target);
+    }
+
+}
