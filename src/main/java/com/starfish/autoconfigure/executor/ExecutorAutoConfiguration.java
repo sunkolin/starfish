3f0f320e36dcc64f00545c3657685aa61ce8b320
sunlin <sunlin@kingsoft.com>
2022-03-15 10:46:26 +0800

    fix

--- src/main/java/com/starfish/autoconfigure/executor/ExecutorAutoConfiguration.java
+++ src/main/java/com/starfish/autoconfigure/executor/ExecutorAutoConfiguration.java
@@ -2,7 +2,9 @@ package com.starfish.autoconfigure.executor;
 
 import com.google.common.base.Strings;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +26,7 @@ import java.util.concurrent.RejectedExecutionHandler;
 @Deprecated
 @Slf4j
 @Configuration
+@AutoConfigureAfter(TaskExecutionAutoConfiguration.class)
 @ConditionalOnProperty(prefix = "application.executor", name = "enabled", havingValue = "true")
 @EnableConfigurationProperties({ExecutorProperties.class})
 public class ExecutorAutoConfiguration {
