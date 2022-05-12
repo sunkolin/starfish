68a92fc74d57895a9347877722556ca107fefbfd
sunlin <sunlin@kingsoft.com>
2022-05-12 16:45:56 +0800

    fix

--- src/test/java/com/starfish/util/MailUtilTest.java
+++ src/test/java/com/starfish/util/MailUtilTest.java
@@ -10,8 +10,8 @@ public class MailUtilTest {
     @Test
     public void testSend() throws MessagingException {
         String receiver = "jinkela008@foxmail.com";
-        String subject = "邮件系统";
-        String content = "你好，此邮件由邮件系统发送。";
+        String subject = "starfish";
+        String content = "你好，此邮件由starfish发送。";
         MailUtil.send(Lists.newArrayList(receiver), null, null, subject, content);
     }
 
