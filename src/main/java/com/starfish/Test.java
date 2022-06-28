a93e04f686d6c96c74ecaa48f5a442e9e680f03f
sunlin <sunlin@kingsoft.com>
2022-06-28 11:03:02 +0800

    升级springboot2.7.1

--- src/main/java/com/starfish/Test.java
+++ src/main/java/com/starfish/Test.java
@@ -1,12 +1,5 @@
 package com.starfish;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
-import com.starfish.util.FileUtil;
-
-import java.util.List;
-
 /**
  * Test
  *
@@ -16,17 +9,8 @@ import java.util.List;
  */
 public class Test {
 
-    public static void main(String[] args) throws JsonProcessingException {
-        List<String> list = FileUtil.readLines("classpath:music.txt");
-        Multiset<String> multiset = HashMultiset.create();
-        multiset.addAll(list);
-        multiset.remove("", Integer.MAX_VALUE);
-        for (String item : multiset) {
-            int count = multiset.count(item);
-            if (count > 1) {
-                System.out.println(item);
-            }
-        }
+    public static void main(String[] args) {
+
     }
 
 }
