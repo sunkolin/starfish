c3e80d2ef6491c9bf71f76c27d663b91ffe20245
sunlin <sunlin@kingsoft.com>
2022-07-15 09:57:10 +0800

    fix

--- src/main/java/com/starfish/core/SpringPlus.java
+++ src/main/java/com/starfish/core/SpringPlus.java
@@ -2,7 +2,6 @@ package com.starfish.core;
 
 import lombok.Data;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
