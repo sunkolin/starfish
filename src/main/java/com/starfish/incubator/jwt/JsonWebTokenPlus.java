4074245fcfc84fea39fdb5667ecc242a2cf65fc9
sunlin <sunlin@kingsoft.com>
2024-06-17 15:00:41 +0800

    优化

--- src/main/java/com/starfish/incubator/jwt/JsonWebTokenPlus.java
+++ src/main/java/com/starfish/incubator/jwt/JsonWebTokenPlus.java
@@ -9,6 +9,9 @@ import java.util.Map;
 
 /**
  * JsonWebTokenPlus
+ * JWT是由三段信息构成的，将这三段信息文本用.链接一起就构成了Jwt字符串。
+ * 就像这样:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjk5fQ.7DwG61h2GjZWA-sSUQA2jFVIenyOhz310rCUbCi09hY
+ * 第一部分我们称它为头部（header),第二部分我们称其为载荷（payload, 类似于飞机上承载的物品)，第三部分是签证（signature).
  *
  * @author sunkolin
  * @version 1.0.0
