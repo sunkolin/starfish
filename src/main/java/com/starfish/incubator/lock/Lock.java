6814581644b893c2149a0125bfa356e902153377
sunlin <sunlin@kingsoft.com>
2024-09-04 15:49:28 +0800

    fix

---
+++ src/main/java/com/starfish/incubator/lock/Lock.java
@@ -0,0 +1,16 @@
+package com.starfish.incubator.lock;
+
+/**
+ * Lock
+ *
+ * @author sunkolin
+ * @version 1.0.0
+ * @since 2024-09-04
+ */
+public interface Lock {
+
+    boolean lock();
+
+    boolean unlock();
+
+}
