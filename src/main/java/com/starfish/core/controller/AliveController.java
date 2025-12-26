a79d6fd2075f435c9995df6940826ea73dd950fa
sunlin <sunlin@kingsoft.com>
2025-12-26 14:26:29 +0800

    update

--- src/main/java/com/starfish/core/controller/AliveController.java
+++ src/main/java/com/starfish/core/controller/AliveController.java
@@ -1,6 +1,7 @@
 package com.starfish.core.controller;
 
 import com.starfish.core.model.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1.0.0
  * @since 2021-06-02
  */
+@Slf4j
 @RestController
 public class AliveController {
 
@@ -19,6 +21,7 @@ public class AliveController {
      */
     @GetMapping({"/api/alive", "/starfish/api/alive", "/${spring.application.name}/api/alive"})
     public Result<String> alive() {
+        log.info("service is alive.");
         return Result.success("alive");
     }
 
