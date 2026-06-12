6970aa1d7afe2996667e7a440faaa9815fb692d6
sunkolin <sunkolin@qq.com>
2026-06-12 16:57:25 +0800

    fix

--- src/main/java/com/starfish/common/cache/redis/RedisService.java
+++ src/main/java/com/starfish/common/cache/redis/RedisService.java
@@ -1,8 +1,6 @@
 package com.starfish.common.cache.redis;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import java.util.concurrent.TimeUnit;
